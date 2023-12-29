35173c319de5162611f396f73156607785b7afb5
Maxime Jambou~ <maxime.jambou@gmail.com>
2023-12-29 16:46:38 +0100

    #1 Bullet points corrected

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -40,12 +40,12 @@ const Slider = () => {
           </div>
           <div className="SlideCard__paginationContainer">
             <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
+              {byDateDesc.map((e, radioIdx) => (
                 <input
-                  key={`${event.id}`}
+                  key={`dot-${event.title}-${e.title}`}
                   type="radio"
                   name="radio-button"
-                  checked={idx === radioIdx}
+                  checked={index === radioIdx}
                 />
               ))}
             </div>
